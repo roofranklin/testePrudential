fa7f75d24cb2a9010f54523b03b6171faa904f5d
frontSinistroRE <roosevelt.santos@mjv.com.br>
2020-04-24 00:05:58 -0300

    Inclusão da lista de planos em angular

---
+++ js/app.module.js
@@ -0,0 +1,3 @@
+var testePrudential = angular.module('testePrudential', []);
+
+
---
+++ js/listPlansController.js
@@ -0,0 +1,17 @@
+testePrudential.controller('listPlans', function($scope, $http){
+
+    
+
+    $scope.plans = [];
+    
+        $http
+            .get("js/json/plans.json")
+            .success(function(data){
+                $scope.plans = data;
+                
+            })
+            .error(function(){
+                alert("Não foi possível carregar os dados");
+            });
+
+});
